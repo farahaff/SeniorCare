6eae702afe446e7df6e8d362429fa3f6e91257cc
roshanighimire <36275068+roshanighimire@users.noreply.github.com>
2018-10-25 00:45:14 +0800

    wordpress theme functions

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Theme functions for NOO JobMonster Child Theme.
+ *
+ * @package    NOO JobMonster Child Theme
+ * @version    1.0.0
+ * @author     Kan Nguyen <khanhnq@nootheme.com>
+ * @copyright  Copyright (c) 2014, NooTheme
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+ * @link       http://nootheme.com
+ */
+
+// An example of how to write your own code in another file:
+// require_once dirname(__FILE__) . 'example-code.php';
+
+// Load language file from child-theme
+// add_action( 'after_setup_theme', 'noo_child_theme_setup' );
+// function noo_child_theme_setup() {
+// 	load_child_theme_textdomain( 'noo', get_stylesheet_directory() . '/languages' );
+// }
